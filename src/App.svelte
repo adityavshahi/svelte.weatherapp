f1060071279dcbebfa3553f9e4ca00602aab6282
adityavshahi <avshahi2001@gmail.com>
2024-05-16 19:22:35 +0530

    info div setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
   import Navbar from "./Components/Navbar.svelte";
+  import WeatherInfo from "./Components/WeatherInfo.svelte";
 </script>
 
 <Navbar />
+<WeatherInfo />
---
+++ src/Components/WeatherInfo.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+
+<style>
+    #weather-info {
+        border: 5px solid black;
+        height: 70vh;
+        width: 50vw;
+        border-radius: 20px;
+    }
+</style>
+
+<div id="weather-info">
+
+</div>
