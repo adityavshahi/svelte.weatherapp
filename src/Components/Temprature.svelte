f69576fa28e64abfc87cdbe899090e187acb3a02
adityavshahi <avshahi2001@gmail.com>
2024-06-10 19:53:51 +0530

    minor font fixes

--- src/Components/Temprature.svelte
+++ src/Components/Temprature.svelte
@@ -6,21 +6,24 @@
 
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
     div {
-        font-family: "Nunito Sans", sans-serif;
-        font-size: 1.8rem;
-        position: absolute;
-        left: 27.5%;
-        top: 23%;
+        font-family: "PT Sans", sans-serif;
+        font-size: 3rem;
         display: flex;
+        justify-content: center;
         flex-direction: column;
-        align-items: flex-start;
     }
 
     h1,p {
         margin: 0;
         padding: 0;
     }
+    h1 {
+        font-size: 7rem;
+    }
+
+    
 </style>
     
 
